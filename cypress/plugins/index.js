406a0f51efeb595d526242c370325f2073b071a6
Antonio <barbak2017@gmail.com>
2021-08-15 17:56:38 +0300

    reporter

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,17 +1,11 @@
+//THIS FILE HAVE TO BE FIXED
 /// <reference types="cypress" />
 
 const cucumber = require('cypress-cucumber-preprocessor').default
 
 const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
 
-// module.exports = (on, config) => {
-//   addMatchImageSnapshotPlugin(on, config)
-// }
-
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
-}
-
-module.exports = (on, config) => {
   addMatchImageSnapshotPlugin(on, config)
 }
